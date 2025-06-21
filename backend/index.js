caca85e8509b24dcd1b7ead1a8bc6642ec866b3a
ompanchwate <ompanchwate2003@gmail.com>
2025-06-21 19:42:49 +0530

    index.js update

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,24 @@
 import express from "express";
-import cors from "cors"; // import cors
+import cors from "cors";
 import RegisterRoutes from "./routes/registerRoutes.js";
 import StudentRoutes from "./routes/studentRoutes.js";
 import CompanyRoutes from "./routes/companyRoutes.js";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001; // Use env PORT for deployment
 
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/", (req, res) => {
+// Only for GET on root path
+app.get("/", (req, res) => {
   res.send("Welcome to the Spark Ed API");
-})
-app.use("/api", RegisterRoutes);
-app.use("/api/student", StudentRoutes)
-app.use("/api/company", CompanyRoutes)
+});
 
+// API routes
+app.use("/api", RegisterRoutes);
+app.use("/api/student", StudentRoutes);
+app.use("/api/company", CompanyRoutes);
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
