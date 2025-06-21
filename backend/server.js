1421befe294d91c7d19e80a294aca20fd760eba3
ompanchwate <ompanchwate2003@gmail.com>
2025-06-21 19:22:50 +0530

    backend home page

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use("/", (req, res) => {
+  res.send("Welcome to the Spark Ed API");
+})
 app.use("/api", RegisterRoutes);
 app.use("/api/student", StudentRoutes)
 app.use("/api/company", CompanyRoutes)
